e7a17be9558dd86a9b5da18e8caf598f8de1a9c6
Tim Thornton <t.thornton6@gmail.com>
2014-12-01 16:51:13 +0000

    Add ability to process and analyze a dump file

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 #include "defs.h"
 #include "entropy.h"
@@ -40,7 +41,15 @@ typedef struct s_packet_distribution {
     unsigned long protocol_flag_class[PFLN];
     double mean;
     double standard_deviation;
+    unsigned long max_count;
+    struct timeval start_time;
+    struct timeval end_time;
 } packet_distribution;
+
+typedef struct s_callback_data {
+    packet_distribution window;
+    packet_distribution baseline;
+} callback_data;
 
 void print_distribution (packet_distribution *distrib)
 {
@@ -68,9 +77,9 @@ double normalized_relative_network_entropy (
     double sum = 0;
     sum += relative_entropy (distrib1->count, distrib1->pkt_len_class,
             distrib2->count, distrib2->pkt_len_class, LENN);
-    sum += relative_entropy (distrib1->count, distrib1->protocol_flag_class,
-            distrib2->count, distrib2->protocol_flag_class, PFLN);
-    return sum / 2;
+//    sum += relative_entropy (distrib1->count, distrib1->protocol_flag_class,
+//            distrib2->count, distrib2->protocol_flag_class, PFLN);
+    return sum / 1;
 }
 
 ProtocolFlag get_protocol_flag(Protocol proto, const struct sniff_tcp *tcp)
@@ -180,7 +189,7 @@ void process_tcp_packet (
     tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
     size_tcp = TH_OFF(tcp) * 4;
     if (size_tcp < 20) {
-        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+        //printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
         return;
     }
 
@@ -202,8 +211,6 @@ void process_tcp_packet (
     //     - if using ncurses, or some real-time view, move this to process_packet
     //       to avoid duplicating in udp()
     //    printf("   Protocol: TCP\n");
-    if (distrib->count % 50 == 0)
-        printf("   Number of packets recorded: %lu\n", distrib->count);
     //    printf("   From            : %s\n", inet_ntoa(ip->ip_src));
     //    printf("   To              : %s\n", inet_ntoa(ip->ip_dst));
     //    printf("   Src port        : %d\n", ntohs(tcp->th_sport));
@@ -248,8 +255,8 @@ void process_tcp_packet (
 // Given a udp packet, update the distribution information `distrib` with the correct classes
 void process_udp_packet () {
     // TODO Must update distribution information in same way as tcp()
-    printf("   Protocol: UDP\n");
-    printf("   Primary class   : %d\n", get_protocol_flag(P_UDP, NULL));
+//    printf("   Protocol: UDP\n");
+ //   printf("   Primary class   : %d\n", get_protocol_flag(P_UDP, NULL));
 }
 
 // Capture a packet from the network, switch on its protocol, and classify its distributoin information.
@@ -267,13 +274,17 @@ void process_packet(
     // Obtain the running distributions from the data (@see s_packet_distribution)
     distrib = (packet_distribution *) data;
 
+    if (distrib->count == 0) {
+        memcpy(&distrib->start_time, &pkthdr->ts, sizeof(struct timeval));
+    }
+
     // Ensure correct packet length (throw away invalid packets)
     // TODO - should the distribution of correct to incorrect packets be logged?
     ethernet = (struct sniff_ethernet*)(packet);
     ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
     size_ip = IP_HL(ip) * 4;
     if (size_ip < 20) {
-        printf("   * Invalid IP header length: %u bytes\n", size_ip);
+    //    printf("   * Invalid IP header length: %u bytes\n", size_ip);
         return;
     }
 
@@ -288,7 +299,6 @@ void process_packet(
             process_udp_packet();
             break;
         case IPPROTO_ICMP:
-            printf("TODO ICMP\n");
             return;
         case IPPROTO_IP:
             return;
@@ -298,8 +308,9 @@ void process_packet(
 
     // TODO remove magic Number
     // stop capturing at the window limit
-    if (distrib->count >= 1000) {
+    if (distrib->count >= distrib->max_count) {
         pcap_breakloop(descr);
+        memcpy(&distrib->end_time, &pkthdr->ts, sizeof(struct timeval));
     }
 }
 
@@ -371,7 +382,7 @@ double std_dev(double *distribution, int num)
     return mean(squares, num);
 }
 
-void capture_regular_deviation(packet_distribution *baseline, int num_windows)
+void capture_regular_deviation(packet_distribution *baseline, int num_windows, int window_size)
 {
     int i;
     double window_history[num_windows];
@@ -380,6 +391,7 @@ void capture_regular_deviation(packet_distribution *baseline, int num_windows)
 
     for (i = 0; i < num_windows; ++i) {
         bzero(&window, sizeof (packet_distribution));
+        window.max_count = window_size;
         printf ("\nBeginning window %d capture of baseline...\n\n", i);
         pcap_loop(descr, -1, process_packet, (u_char *)&window);
 
@@ -389,6 +401,7 @@ void capture_regular_deviation(packet_distribution *baseline, int num_windows)
         print_distribution(&window);
         if (!distribution_correct) {
             fprintf(stderr, "Window distribution is not correct\n");
+            print_distribution(&window);
             exit(1);
         } else {
             // The distribution is correct in this case, so proceed...
@@ -402,6 +415,7 @@ void capture_regular_deviation(packet_distribution *baseline, int num_windows)
     baseline->standard_deviation = std_dev(window_history, num_windows);
 }
 
+
 int main(int argc, char **argv)
 {
     char *dev;
@@ -411,6 +425,8 @@ int main(int argc, char **argv)
     double nrne;
     double std;
     unsigned char distribution_correct;
+    double max_dev = 0;
+    int num_attacks = 0;
 
     // Initialize the running distribution
     baseline_distribution = (packet_distribution *) malloc ( sizeof (packet_distribution) );
@@ -419,14 +435,15 @@ int main(int argc, char **argv)
     bzero(window_distribution, sizeof (packet_distribution));
 
     // Find a device to sniff
-    dev = pcap_lookupdev(errbuf);
-    if (dev == NULL) {
-        printf("%s\n", errbuf);
-        exit(1);
-    }
+    //dev = pcap_lookupdev(errbuf);
+    //if (dev == NULL) {
+    //    printf("%s\n", errbuf);
+    //    exit(1);
+    //}
 
     // Open the interface
-    descr = pcap_open_live(dev, BUFSIZ, 0, -1, errbuf);
+//    descr = pcap_open_live(dev, BUFSIZ, 0, -1, errbuf);
+    descr = pcap_open_offline("testdata/attack", errbuf);
     if (descr == NULL) {
         printf("pcap_open_live(): %s\n", errbuf);
         exit(1);
@@ -434,6 +451,7 @@ int main(int argc, char **argv)
 
     // Begin capturing packets for baseline behaviour
     printf("Starting baseline capture\n");
+    baseline_distribution->max_count = 100000000;
     pcap_loop(descr, -1, process_packet, (u_char *)baseline_distribution);
     distribution_correct = sanity_check_distribution(baseline_distribution);
     if (!distribution_correct) {
@@ -441,41 +459,76 @@ int main(int argc, char **argv)
         exit(1);
     }
 
+    pcap_close(descr);
+    descr = pcap_open_offline("testdata/attack", errbuf);
+    if (descr == NULL) {
+        printf("pcap_open_live(): %s\n", errbuf);
+        exit(1);
+    }
+
+    capture_regular_deviation(baseline_distribution, floor(baseline_distribution->count / 50000), 50000);
+
     printf ("\nFinished capturing baseline behaviour.\n\n");
-    capture_regular_deviation(baseline_distribution, 5);
     print_distribution(baseline_distribution);
-    printf("Regular deviation: %f\n", baseline_distribution->standard_deviation);
+    printf("  Between %s      and %s\n",
+            ctime((const time_t*)&baseline_distribution->start_time.tv_sec),
+            ctime((const time_t*)&baseline_distribution->end_time.tv_sec));
+    printf("Regular deviation: %f\n\n", baseline_distribution->standard_deviation);
+
+    pcap_close(descr);
+    descr = pcap_open_offline("testdata/attack", errbuf);
+    if (descr == NULL) {
+        printf("pcap_open_live(): %s\n", errbuf);
+        exit(1);
+    }
 
     // Begin capturing window distributions and comparing every `window_size_in_pkts` many captures
     for (;;) {
-        printf ("\nBeginning windowed capture...\n\n");
+//        printf ("\nBeginning windowed capture...\n\n");
+
+        // Reset the window distribution
+        bzero(window_distribution, sizeof (packet_distribution));
+        window_distribution->max_count = 50000;
         pcap_loop(descr, -1, process_packet, (u_char *)window_distribution);
+
+        // If we have finished reading the file, break out of the loop
+        if (window_distribution->count < window_distribution->max_count) {
+            break;
+        }
 
         // Sanity check, ensure all probability vectors sum to 1
         //        Kullback-Leibler divergence is only defined at this point
         distribution_correct = sanity_check_distribution(window_distribution);
         if (!distribution_correct) {
             fprintf(stderr, "Window distribution is not correct, ignoring window\n");
+            print_distribution(window_distribution);
         } else {
             // The distribution is correct in this case, so proceed...
             // Calculate the relative entropy of the two distributions
-            relative_entropy_of_distributions(window_distribution, baseline_distribution);
+            //relative_entropy_of_distributions(window_distribution, baseline_distribution);
             nrne = normalized_relative_network_entropy( window_distribution, baseline_distribution);
             std  = square_difference(nrne, baseline_distribution->mean);
-            printf("NRNE: %lf\n", nrne);
-            printf("Deviation: Regular(%f) Window(%f)\n", baseline_distribution->standard_deviation, std);
-            print_distribution(baseline_distribution);
+            if (std > max_dev) { max_dev = std; }
+//            printf("    NRNE: %lf\n", nrne);
+            //printf("    Deviation: Regular(%f) Window(%f)\n", baseline_distribution->standard_deviation, std);
+            //print_distribution(baseline_distribution);
 
-            if (std > 1) { //(3 * baseline_distribution->standard_deviation)) {
-                printf("[!!] ANOMALOUS USAGE DETECTED!!!");
+            if (std > (2 * baseline_distribution->standard_deviation)) {
+                printf("[!!] ANOMALOUS USAGE DETECTED!!!\n");
+                printf("  Deviation: Regular(%f) Window(%f)\n", baseline_distribution->standard_deviation, std);
+                printf("  Between %s      and %s\n",
+                        ctime((const time_t*)&window_distribution->start_time.tv_sec),
+                        ctime((const time_t*)&window_distribution->end_time.tv_sec));
+                num_attacks++;
             }
         }
-
-        // Reset the window distribution
-        bzero(window_distribution, sizeof (packet_distribution));
     }
 
+    printf("Max deviation: %f\n", max_dev);
+    printf("Num attacks  : %d\n", num_attacks);
+
     // This will never be reached
+    pcap_close(descr);
     free (baseline_distribution);
     free (window_distribution);
     return 0;
